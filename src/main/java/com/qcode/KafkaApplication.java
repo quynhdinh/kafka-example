c88228b2f4a90a51749bb185e8c7cc26f1a1c001
quynhdv <quynhdv@vng.com.vn>
2022-06-01 18:33:02 +0700

    complete

--- src/main/java/com/qcode/KafkaApplication.java
+++ src/main/java/com/qcode/KafkaApplication.java
@@ -15,6 +15,9 @@ public class KafkaApplication {
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
             kafkaTemplate.send("qcode", "hello kafka");
+            for(int i = 0; i < 10_000_000; i++){
+            kafkaTemplate.send("amigoscode", "hello kafka :) " + i);
+            }
         };
     }
 }
---
+++ src/main/java/com/qcode/MessageRequest.java
@@ -0,0 +1,4 @@
+package com.qcode;
+
+public record MessageRequest(String message) {
+}
